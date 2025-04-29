99086c69e0cf058479d1d16d3eb6bd025cde8298
La-guajolota <adriansilpa@gmail.com>
2025-04-29 12:30:38 -0600

    Added logic for servo motor

--- LiDAR_TFluna/LiDAR_TFluna.c
+++ LiDAR_TFluna/LiDAR_TFluna.c
@@ -7,14 +7,22 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/irq.h"
+#include "hardware/pwm.h"
 
 // Include the header file for the TF-Luna sensor.
 #include "tf_luna.h"
 
+// Intances
 tf_luna_t LiDAR;
 
-volatile bool data_ready = true; // Flag to indicate if new data is ready.
+// Prototypes
+#define servo_pwm 0  // PWM channel for servo control
+#define servo_gpio 0 // GPIO pin for servo control
+int pos = 0;
+bool going_up = true;
+void move_servo(void);
 
+volatile bool data_ready = true; // Flag to indicate if new data is ready.
 /**
  * @brief GPIO interrupt callback function.
  *
@@ -33,22 +41,24 @@ void gpio_callback(uint gpio, uint32_t events)
 
 int main()
 {
-    // Initialize stdio for USB output.
     stdio_init_all();
+
+    // Initialize PWM for servo control.
+    gpio_set_function(servo_gpio, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(servo_gpio);
+    pwm_config config = pwm_get_default_config(); // Get the default PWM configuration.
+    pwm_config_set_clkdiv(&config, 4.f);          // Set the clock divider to 4.
+    pwm_init(slice_num, &config, true);           // Initialize PWM with the configuration.
 
     // Initialize I2C.
     i2c_init(I2C_PORT, 400 * 1000); // Set clock to 400 kHz.
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    // Uncomment if there are no external 4.7k pull-up resistors on the board.
-    // gpio_pull_up(I2C_SDA);
-    // gpio_pull_up(I2C_SCL);
 
     // Initialize GPIO for the "data ready" signal.
     gpio_init(TF_LUNA_MUX_OUT);
     gpio_set_dir(TF_LUNA_MUX_OUT, GPIO_IN);
-    gpio_set_pulls(TF_LUNA_MUX_OUT, false, false); // Disable internal pull-up/down resistors.
-    // Uncomment if you want to use the internal pull-up resistor.
+    gpio_set_pulls(TF_LUNA_MUX_OUT, false, false);
     // gpio_pull_up(TF_LUNA_MUX_OUT); // Pull-up to 3.3V.
 
     // Configure interrupt for rising edge on the "data ready" signal.
@@ -61,6 +71,33 @@ int main()
             data_ready = false;             // Clear the flag.
             get_distance(&LiDAR);           // Read the distance value.
             printf("%d\n", LiDAR.distance); // Print the distance value.
+            move_servo();                   // Move servo motor.
+            sleep_ms(250);                  // Delay x ms.
         }
     }
+}
+
+// Function to move the servo motor.
+void move_servo(void)
+{
+    if (going_up)
+    {
+        ++pos;
+        if (pos > 255)
+        {
+            pos = 255;
+            going_up = false;
+        }
+    }
+    else
+    {
+        --pos;
+        if (pos < 0)
+        {
+            pos = 0;
+            going_up = true;
+        }
+    }
+
+    pwm_set_gpio_level(servo_gpio, pos * pos);
 }
